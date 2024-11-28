826db376164f4fed4d9bc23ecda6d7dca737839e
Whitewolf24 <geo_marinos@yahoo.com>
2024-11-28 03:34:22 +0200

    1.2 "b2 test (working)"

--- resources/views/eknexa/index.blade.php
+++ resources/views/eknexa/index.blade.php
@@ -1,10 +1,16 @@
-@foreach ($posts as $post)
-<div class="yliko{{ $loop->last ? ' last' : '' }}">
-    <h1>{{ $post->title }}</h1>
-    @if ($post->image_path)
-    <!-- Use the public URL directly -->
-    <img src="{{ 'https://f000.backblazeb2.com/file/' . env('B2_BUCKET_NAME') . '/' . $post->image_path }}" alt="{{ $post->title }}">
-    @endif
-    <p>{!! nl2br($post->content) !!}</p>
-</div>
-@endforeach
+@extends('layouts.eknexa_lay')
+
+@section('content')
+<main>
+    @foreach ($posts as $post)
+    <div class="yliko{{ $loop->last ? ' last' : '' }}">
+        <h1>{{ $post->title }}</h1>
+        @dd($post) <!-- Add this to debug the content -->
+        @if ($post->image_path)
+        <img src="{{ 'https://f000.backblazeb2.com/file/' . env('B2_BUCKET_NAME') . '/' . $post->image_path }}" alt="{{ $post->title }}">
+        @endif
+        <p>{!! nl2br($post->content) !!}</p>
+    </div>
+    @endforeach
+</main>
+@endsection
