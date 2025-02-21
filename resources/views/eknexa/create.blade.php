ac49056fa83d5f9349b4192cffe445cdea5622a6
Whitewolf24 <geo_marinos@yahoo.com>
2025-02-22 01:57:26 +0200

    1.7 "added title missing warning"

--- resources/views/eknexa/create.blade.php
+++ resources/views/eknexa/create.blade.php
@@ -5,7 +5,7 @@
     @csrf
     <div id="title">
         <label>Title:</label>
-        <input type="text" name="title" value="{{ old('title') }}">
+        <input type="text" name="title" value="{{ old('title') }}" required>
         @error('title') <div class="error">{{ $message }}</div> @enderror
     </div>
     <div id="content">
@@ -27,20 +27,29 @@
         const form = document.getElementById('form');
         if (form) {
             form.addEventListener('submit', function(event) {
-                const file_input = document.getElementById('img_upload');
-                const file = file_input.files[0];
+                const title = document.getElementById('title');
+                const img_upload = document.getElementById('img_upload');
+                const file = img_upload.files[0];
+
+                if (!title.value.trim()) {
+                    alert("Παρακαλώ συμπληρώστε τίτλο");
+                    event.preventDefault(); 
+                    return;
+                }
+
 
                 if (file) {
-                    if (file.size > 10 * 1024 * 1024) {
-                        alert("Παρακαλώ επιλέξτε μια εικόνα με μέγεθος έως 10MB.");
-                        event.preventDefault(); 
-                        return;
-                    }
-
                     const valid_types = ['image/jpeg', 'image/jpg', 'image/webp', 'image/png'];
                     if (!valid_types.includes(file.type)) {
                         alert("Παρακαλώ επιλέξτε μια έγκυρη εικόνα (jpg, jpeg, webp, png).");
-                        event.preventDefault(); 
+                        event.preventDefault();
+                        return;
+                    }
+
+                    if (file.size > 10 * 1024 * 1024) {
+                        alert("Παρακαλώ επιλέξτε μια εικόνα με μέγεθος έως 10MB.");
+                        event.preventDefault();
+                        return;
                     }
                 }
             });
